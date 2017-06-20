fb4d7fef0d485dbaddab0425e369ec97b1cbe3b0
rob alexander <itsticksco@gmail.com>
2017-06-20 23:03:52 +0100

    turned the volume down

--- js/player.js
+++ js/player.js
@@ -12,20 +12,19 @@ class GamePiece {
 }
 class Player extends GamePiece {
     constructor() {
-            super();
-	// this.x = gridCellSquarePixels;
-	// this.y = gridCellSquarePixels;
- //    this.width = 10;
- //    this.height = 10;
+    super();
     this.color = '#ffffff';
 	this.direction = directions[0];
 	this.speed = speeds[0];
 
     this.audioCtx = new AudioContext;
+    this.gain = this.audioCtx.createGain();
+    this.gain.gain.value = 0.005;
     this.oscillator = this.audioCtx.createOscillator();
     this.oscillator.start(0);
     this.oscillator.frequency.value = this.y;
-    this.oscillator.connect(this.audioCtx.destination);
+    this.oscillator.connect(this.gain)
+    this.gain.connect(this.audioCtx.destination);
 
     this.forward = () => {
         switch(this.direction) {
