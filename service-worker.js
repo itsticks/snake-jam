d5b08b490c854bc1fe78646496f886fa75c79005
rob alexander <itsticksco@gmail.com>
2017-07-09 21:44:01 +0100

    fullscreen and black app colouring

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/',
         'index.html',
         'js/Grid.js',
         'js/play.js',
